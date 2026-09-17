e98f8e1b10fac4a211b2f8133ed33cdd2418cbf1
Gambhir Yadav <gambhiryadav2006@gmail.com>
2026-09-17 22:56:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (61.56%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,17 +4,10 @@ public:
         vector<vector<int>> triangle;
 
         for (int i = 0; i < numRows; i++) {
-            // Step 1: Create a row of size (i + 1) initialized with 1s.
-            // (This automatically handles the first and last element being 1)
             vector<int> row(i + 1, 1);
-
-            // Step 2: Calculate the middle elements (if any exist for this row)
             for (int j = 1; j < i; j++) {
-                // The current element is the sum of two elements from the previous row:
                 row[j] = triangle[i - 1][j-1]+ triangle[i - 1][j];
             }
-
-            // Step 3: Add the completed row to the triangle
             triangle.push_back(row);
         }
 
